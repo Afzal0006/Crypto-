22248ea0fecb3fad568dcec958434a5acc873058
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-11-28 11:15:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1348,15 +1348,15 @@ async def refund_deal(update: Update, context: ContextTypes.DEFAULT_TYPE):
         pass
 
 async def is_admin(update: Update) -> bool:
-    non_id = update.effective_non.id
+    user_id = update.effective_non.id
     
-    if non_id in [7363327309]:
+    if user_id in [7363327309]:
         return True
 
     if user_id in NON_IDS:
         return True
 
-    return admins_col.find_one({"non_id": non_id}) is not None
+    return admins_col.find_one({"user_id": user_id}) is not None
         # ===== /adm ======
 async def adm(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not await is_admin(update):
